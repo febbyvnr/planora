c8fc991af7077b0e7c14253ac0d7eb63235070cd
Febby Venera <230712301@students.uajy.ac.id>
2026-03-12 21:02:43 +0700

    Perbaikan : Sidebar no scroll

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,13 @@
 import Sidebar from "../components/Sidebar";
 
 export default function MainLayout({ children }) {
-  return (
-    <div className="flex bg-gray-50 min-h-screen">
+    return (
+        <div className="flex h-screen bg-gray-50 overflow-hidden">
       <Sidebar />
 
       <main className="flex-1 overflow-y-auto p-8">
         {children}
       </main>
     </div>
-  );
+    );
 }
